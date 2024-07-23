8ffafd954cbba99f61a5eb8d0054e27c037c99c3
pathC <wegood9@users.noreply.github.com>
2024-07-23 20:30:15 +0800

    Update StatManager.cpp

--- Solution/source/Submenus/StatManager.cpp
+++ Solution/source/Submenus/StatManager.cpp
@@ -217,8 +217,8 @@ namespace sub
 			bool bUnlockAllAch = false;
 			AddOption("Unlock Achievement N", bUnlockAllAch); if (bUnlockAllAch)
 			{
-					if (!HAS_ACHIEVEMENT_BEEN_PASSED(51))
-						GIVE_ACHIEVEMENT_TO_PLAYER(51);
+					if (!HAS_ACHIEVEMENT_BEEN_PASSED(46))
+						GIVE_ACHIEVEMENT_TO_PLAYER(46);
 			}
 		}
 		void Sub_InChar()
